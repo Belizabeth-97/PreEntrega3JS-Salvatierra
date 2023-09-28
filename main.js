7692e9c3aa0276065e18773522245af4ab743b15
Brenda Salvatierra <brendaelizabethsalvatierra@gmail.com>
2023-09-28 00:43:44 -0300

    Agregando boton eliminar del carrito

--- main.js
+++ main.js
@@ -50,6 +50,7 @@ class ControladorProducto{
 
       AniadirProducto.addEventListener("click", () => {
         carrito.agregar(producto);
+        carrito.guardarCarritoEnLocalStorage()
         carrito.mostrar();
       });
     });
@@ -59,11 +60,17 @@ class ControladorProducto{
 class Carrito {
     constructor(){
     this.listaCarrito = [];
+    this.recuperarStorage();
   }
 
   agregar(producto){
     this.listaCarrito.push(producto);
     this.guardarCarritoEnLocalStorage();
+  }
+
+  eliminar(productoEliminado){
+    let indice = this.listaCarrito.findIndex(producto => producto.id == productoEliminado.id) 
+    this.listaCarrito.splice(indice, 1)
   }
 
   guardarCarritoEnLocalStorage(){
@@ -94,11 +101,30 @@ class Carrito {
                 <h5 class="card-title">${producto.nombre}</h5>
                 <p class="card-text">Cantidad: ${producto.cantidad}</p>
                 <p class="card-text">Precio: $${producto.precio}</p>
+                <button class="btn" id= "ep-${producto.id}">  
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                        <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
+                    </svg>  
+                </button>            
               </div>
             </div>
           </div>
         </div>`;
     });
+
+    this.listaCarrito.forEach((producto) => {
+      let  btn_eliminar = document.getElementById(`ep-${producto.id}`)
+    
+      btn_eliminar.addEventListener("click", () => {
+        
+        this.eliminar(producto)
+        this.guardarCarritoEnLocalStorage()
+        this.mostrar()
+      
+      })
+    });
+
+
   }
 }
 
